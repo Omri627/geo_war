f1d5dfb6dda98dfddede4c13240c640600c699ff
Omri Sak <omri.sak@live.biu.ac.il>
2020-11-28 17:21:28 +0200

    user class

---
+++ backend/db/dal_quries/user_queries.py
@@ -0,0 +1,11 @@
+from db.dal_quries.table_queries import TableQueries
+
+
+class LanguagesQueries(TableQueries):
+    FIELDS = (
+        'user_name', 'password')
+
+    INSERT_QUERY = '''
+        INSERT INTO users (user_name, password)
+            VALUES (%s, %s)
+    '''
